7132e43aacf5c612db1481b16d7d1803a91faa44
TANISHGM <gmtanishkumar1110@gmail.com>
2026-01-03 17:39:25 +0530

    new

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`<h1>🚀 Deployed via CI/CD!</h1><p>${new Date()}</p>`);
+});
+
+app.listen(3000, () => console.log("App running on port 3000"));
