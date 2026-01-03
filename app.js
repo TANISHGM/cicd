b336098402b7b348a0e16f2bd7e8be4bcefffc33
TANISHGM <gmtanishkumar1110@gmail.com>
2026-01-03 18:35:15 +0530

    hiugh

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(`<h1>🚀 Deployed via CI/CD!</h1><p>${new Date()}</p>`);
-  res.send(`<h1>🚀jsut f**ked upp???/CI/CD!</h1><p>${new Date()}</p>`);
+  res.send(`<h1>🚀 Happy deloyedment via CI/CD!</h1><p>${new Date()}</p>`);
+  
 });
 
 app.listen(3000, () => console.log("App running on port 3000"));
